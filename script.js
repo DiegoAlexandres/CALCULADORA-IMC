951a20127cb74beebb18c5df24d6b7999fcd1544
DiegoAleandres <diegoalexandre.me@gmail.com>
2024-01-16 01:01:30 -0300

    Iniciando o calculo de IMC com javascript

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
 const btnCalculate = document.querySelector(".btn-calculate")
 const modalWrapper = document.querySelector(".modal-wrapper")
 const btnModal = document.querySelector(".close")
 
 btnCalculate.addEventListener("click", handleClick)
-btnModal.addEventListener("click", handleClick)
+btnModal.addEventListener("click", handleClickModal)
+
+// form.onsubmit = handleClick() 
 
 function handleClick(event){
     event.preventDefault()
 
-    modalWrapper.classList.toggle("open")
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    let IMC = weight / height**2
+
+    console.log(IMC)
+    
+}
+
+function handleClickModal(event){
+    event.preventDefault()
+
+    modalWrapper.classList.toggle("open") 
 }
